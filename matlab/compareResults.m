908f8b3e740d16156d1a79029594f2f6534198ac
hamed <amzhamed@gmail.com>
2022-11-14 09:03:40 -0800

    refine compare results

--- matlab/compareResults.m
+++ matlab/compareResults.m
@@ -1,9 +1,13 @@
 function compareResults()
     load('result1.mat', 'result1');
     load('result2.mat', 'result2');
+    load('result3.mat', 'result3');
+    load('result4.mat', 'result4');
 
-    plotResults(result1(1,:), result2(1,:), 'Number of Frozen FLS', 4);
-    plotResults(result1(2,:), result2(2,:), 'Number of Moved FLS', 5);
+    plotResults('Minimum Distance Traveled', 4, result1(18,:), 'Signal Strength', result2(18,:), 'Max Radius', result3(18,:), 'Avg Radius', result4(18,:), 'Random');
+    plotResults('Average Distance Traveled', 5, result1(3,:), 'Signal Strength', result2(3,:), 'Max Radius', result3(3,:), 'Avg Radius', result4(3,:), 'Random');
+    plotResults('Maximum Distance Traveled', 6, result1(4,:), 'Signal Strength', result2(4,:), 'Max Radius', result3(4,:), 'Avg Radius', result4(4,:), 'Random');
+    plotResults('Number of Moving FLSs', 7, result1(2,:), 'Signal Strength', result2(2,:), 'Max Radius', result3(2,:), 'Avg Radius', result4(2,:), 'Random');
 %     nexttile
 %     plotResults(result1(5,:), result2(5,:), 'Hausdorff');
 %     nexttile
