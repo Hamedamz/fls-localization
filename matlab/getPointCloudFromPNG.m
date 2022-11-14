f13b47c2b50f253bc5a82734995c8870f487621d
hamed <amzhamed@gmail.com>
2022-11-14 09:08:05 -0800

    fix png to point cloud

--- matlab/getPointCloudFromPNG.m
+++ matlab/getPointCloudFromPNG.m
@@ -7,9 +7,12 @@ count = 0;
 xres = size(transparency, 1);
 yres = size(transparency, 2);
 
+pointCloud = [];
+
 for i = 1:xres
     for j = 1:yres
-        if transparency(i, j) > 127
+        t = transparency(i, j);
+        if (t < 1 && t > 0.25) || (t > 127)
             count = count + 1;
             pointCloud(:, count) = [j + 3; xres - i + 3];
         end
