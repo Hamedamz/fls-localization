6a1c07b1ff1293b65c66dda36409215040d38213
hamed <amzhamed@gmail.com>
2022-11-21 15:10:59 -0800

    add save fig script

--- matlab/saveFigs.m
+++ matlab/saveFigs.m
@@ -6,3 +6,5 @@ for iFig = 1:length(FigList)
   s=sprintf('./figs/fig%d.jpg', iFig);
   saveas(FigHandle, s);
 end
+
+% https://docs.google.com/presentation/d/16SekQjgqeR-YO95KRU49CXiBvl7aWfwLhwe1GV46iR4/edit?usp=sharing
